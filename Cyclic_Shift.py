4d1b134a65f833513b38ee2f31e5630252b0ecad
Maneesh Yadav <9890015099y@gmail.com>
2021-04-05 18:30:44 +0530

    Create Cyclic_Shift.py

---
+++ Cyclic_Shift.py
@@ -0,0 +1,28 @@
+def cyclic_shift(N,K):
+    A = input()
+    wer = []
+    count = 0
+    prev = 0
+    B=A[N-1] + A[:N-1]
+    print()
+
+    while(1):
+        A = A[1:N]+A[0]
+        count += 1
+            
+        if B == A:
+            prev += 1
+            wer.append(count) 
+            count = 0
+            
+            if prev==K:
+                break
+                
+    return sum(wer)
+
+
+T = int(input(""))
+for t in range(T):    
+    N , K  = map(int,input().split())
+    print(cyclic_shift(N,K))
+
