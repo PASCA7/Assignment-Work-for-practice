46d9d2d844d37b3c2929396eb78975f9aa35d885
Maneesh Yadav <9890015099y@gmail.com>
2021-11-26 02:23:47 +0530

    Add files via upload
    Maximum Beauty Subarray Question 1.

---
+++ Infinx Data Structure and Algoritm Hiring Test/Programing Data Structure and Algoritm INFINX.py
@@ -0,0 +1,29 @@
+def maxBeauty (N, A):
+    # Write your code here
+    arr=[]
+    for i in range(len(A)-1,-1,-1):
+        j=0
+        while(j<len(A)):
+            if j>len(A):
+                break
+                asd = []
+                for k in range(j,i+1):
+                    asd.append(A[k])
+                    j+=1
+                    arr.append(asd)
+    arr=[x for x in arr if x!= []]
+    res=[]
+    for i in arr:
+        d=0
+        for j in range(len(i)):
+            d=i[j]+d
+        res.append(d*max(i))
+    print(max(res))
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    out_ = maxBeauty(N, A)
+    print (out_)
